579929a46b60d5836975f04dde6c0f25a885061b
osinski <andres.osinski@gmail.com>
2020-08-03 00:36:59 -0300

    Generate nice CSV file

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,34 @@ use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::time::SystemTime;
 
+#[macro_use]
+use serde::Serialize;
+extern crate serde_millis;
+
+use csv::{Writer, WriterBuilder};
+use hex;
 use sha1::{Digest, Sha1};
 use walkdir::{DirEntry, WalkDir};
 
-
+#[derive(Serialize)]
 struct Doc {
-    hash: [u8; 20],
+    hash: String,
     name: String,
-    path: PathBuf,
+    path: String,
+    #[serde(with = "serde_millis")]
     mod_date: SystemTime
 }
 
 fn help() {
     println!("Usage: 
 path - The directory path in which to track file changes");
+}
+
+
+fn create_csv_writer(path: &String) -> csv::Result<Writer<File>> {
+    let path = path.to_owned() + ".dirdiff.csv";
+    println!("Creating CSV {}", &path);
+    WriterBuilder::new().has_headers(false).from_path(path)
 }
 
 
@@ -30,8 +44,12 @@ fn is_hidden(entry: &DirEntry) -> bool {
          .unwrap_or(false)
 }
 
-fn gen_dir_struct(path: &String) -> io::Result<()> {
+fn gen_dir_struct(path: &String) -> io::Result<Vec<Doc>> {
+    let mut dir_entries = Vec::new();
+
+    let mod_date = SystemTime::now();
     let path = Path::new(path);
+
     println!("Getting files from directory {}", &path.display());
 
     if !path.is_dir() {
@@ -40,26 +58,60 @@ fn gen_dir_struct(path: &String) -> io::Result<()> {
     }
 
     for entry in WalkDir::new(path).into_iter().filter_entry(|e| !is_hidden(e)) {
-        let mut data = Vec::new();
+        if let Ok(dir_entry) = entry {
+            let path = &dir_entry.path();
+            let dir_path = path.display();
 
-        let mut fd = File::open(&entry?.path()).unwrap();
+            if dir_entry.metadata()?.file_type().is_dir() {
+                println!("Entry {} is dir!", dir_path);
+                continue
+            }
 
-        fd.read_to_end(&mut data)?;
+            let mut data = Vec::new();
 
-        let hash = Sha1::digest(&data);
-        println!("{}", entry?.path().display());
-        println!("{:?}", hash.as_slice());
+            let mut fd = File::open(dir_entry.path()).unwrap();
+
+            fd.read_to_end(&mut data)?;
+
+            let hash = Sha1::digest(&data);
+
+            println!("{}", &dir_path);
+
+            dir_entries.push(Doc {
+                hash: hex::encode(hash),
+                name: String::from(dir_entry.file_name().to_str().unwrap()),
+                path: String::from(path.to_str().unwrap()),
+                mod_date: mod_date
+            });
+
+        } else {
+            println!("Weird directory name");
+        }
     }
 
-    Ok(())
+    Ok(dir_entries)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();  
 
     match args.len() {
-        1 => { help(); }
-        2 => { gen_dir_struct(&args[1]); }
+        2 => { 
+            let root = &args[1];
+
+            match gen_dir_struct(root) {
+                Err(error) => 
+                    println!("Messed up here: {}", &error),
+                Ok(dir_entries) => {
+                    let mut writer = create_csv_writer(root).expect("Error creating CSV writer");
+                    for entry in dir_entries {
+                        writer.serialize(entry).expect("Error writing CSV record");
+                    }
+                    writer.flush().unwrap();
+                }
+            }
+
+        }
         _ => { help(); }
     }
 }
